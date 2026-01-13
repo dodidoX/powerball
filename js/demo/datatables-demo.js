c440561ea71b712f3c1ccf589482f1b3ac6797cd
dobidoX <yjh3857@gmail.com>
2026-01-13 23:59:01 +0900

    역대 로또 당첨번호 테이블 추가

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,11 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    columnDefs: [
+      {
+        targets: 9, // 1등 당첨금액(원)
+        render: $.fn.dataTable.render.number(',', '.', 0)
+      }
+    ]
+  });
 });
